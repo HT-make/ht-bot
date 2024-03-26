6bd1fdecd3544542219f5d2f1109ae3f1e78be3e
hothuuk <hothuuk@gmail.com>
2024-03-26 20:49:24 +0900

    Initial commit

---
+++ src/main/java/com/htmake/htbot/HtbotApplication.java
@@ -0,0 +1,13 @@
+package com.htmake.htbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HtbotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HtbotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/htmake/htbot/HtbotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.htmake.htbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HtbotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
