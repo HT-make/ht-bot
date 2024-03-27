d751d92589dc9b3849a06adc5eca2f22fb2960fd
hothuuk <hothuuk@gmail.com>
2024-03-27 23:26:24 +0900

    create :: PlayerJoinRequest

---
+++ src/main/java/com/htmake/htbot/domain/player/presentation/data/request/PlayerJoinRequest.java
@@ -0,0 +1,19 @@
+package com.htmake.htbot.domain.player.presentation.data.request;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class PlayerJoinRequest {
+
+    @NotBlank(message = "id는 필수 입니다.")
+    private String userId;
+
+    @NotBlank(message = "이름은 필수 입니다.")
+    private String name;
+}
