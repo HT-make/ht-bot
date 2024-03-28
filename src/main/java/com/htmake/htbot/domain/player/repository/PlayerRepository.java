ac734bd679505c4d7947c79d24b4be4f3bbaa328
hothuuk <hothuuk@gmail.com>
2024-03-28 15:14:23 +0900

    add :: findById 추가

--- src/main/java/com/htmake/htbot/domain/player/repository/PlayerRepository.java
+++ src/main/java/com/htmake/htbot/domain/player/repository/PlayerRepository.java
@@ -3,7 +3,11 @@ package com.htmake.htbot.domain.player.repository;
 import com.htmake.htbot.domain.player.entity.Player;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface PlayerRepository extends JpaRepository<Player, Long> {
 
     Boolean existsById(String id);
+
+    Optional<Player> findById(String id);
 }
