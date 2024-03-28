b98bcabea16f1de737c06c58a684f458d8a58697
hothuuk <hothuuk@gmail.com>
2024-03-28 18:10:32 +0900

    create :: Pair

---
+++ src/main/java/com/htmake/htbot/interfaces/Pair.java
@@ -0,0 +1,30 @@
+package com.htmake.htbot.interfaces;
+
+import java.util.Map;
+
+public class Pair<K, V> implements Map.Entry<K, V> {
+    private final K key;
+    private V value;
+
+    public Pair(K key, V value) {
+        this.key = key;
+        this.value = value;
+    }
+
+    @Override
+    public K getKey() {
+        return key;
+    }
+
+    @Override
+    public V getValue() {
+        return value;
+    }
+
+    @Override
+    public V setValue(V value) {
+        V oldValue = this.value;
+        this.value = value;
+        return oldValue;
+    }
+}
