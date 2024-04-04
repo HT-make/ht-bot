32f3b5cf1c55cb00737bd7eeb1d914dcf9d54cb8
hothuuk <hothuuk@gmail.com>
2024-04-04 10:14:14 +0900

    add :: dropItemList 추가

--- src/main/java/com/htmake/htbot/domain/dungeon/service/impl/MonsterLootServiceImpl.java
+++ src/main/java/com/htmake/htbot/domain/dungeon/service/impl/MonsterLootServiceImpl.java
@@ -1,6 +1,7 @@
 package com.htmake.htbot.domain.dungeon.service.impl;
 
 import com.htmake.htbot.domain.dungeon.entity.Monster;
+import com.htmake.htbot.domain.dungeon.presentation.data.response.DropItemResponse;
 import com.htmake.htbot.domain.dungeon.presentation.data.response.MonsterLootResponse;
 import com.htmake.htbot.domain.dungeon.repository.MonsterRepository;
 import com.htmake.htbot.domain.dungeon.service.MonsterLootService;
@@ -10,6 +11,7 @@ import org.springframework.transaction.annotation.Transactional;
 
 import java.net.URLDecoder;
 import java.nio.charset.StandardCharsets;
+import java.util.stream.Collectors;
 
 @Service
 @Transactional(readOnly = true)
@@ -26,12 +28,14 @@ public class MonsterLootServiceImpl implements MonsterLootService {
         Monster monster = monsterRepository.findByName(decodedName)
                 .orElseThrow();
 
-        String exp = String.valueOf(monster.getExp());
-        String gold = String.valueOf(monster.getGold());
-
         return MonsterLootResponse.builder()
-                .exp(exp)
-                .gold(gold)
+                .exp(monster.getExp())
+                .gold(monster.getGold())
+                .dropItemList(
+                        monster.getDropItems().stream()
+                                .map(DropItemResponse::toResponse)
+                                .collect(Collectors.toList())
+                )
                 .build();
     }
 }
