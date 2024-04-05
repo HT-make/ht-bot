1a70d99f0add6590ccad61414c68f67b4b2f2379
hothuuk <hothuuk@gmail.com>
2024-04-05 22:14:18 +0900

    update :: 기본 Id String으로 변경

--- src/main/java/com/htmake/htbot/domain/player/repository/PlayerRepository.java
+++ src/main/java/com/htmake/htbot/domain/player/repository/PlayerRepository.java
@@ -3,11 +3,5 @@ package com.htmake.htbot.domain.player.repository;
 import com.htmake.htbot.domain.player.entity.Player;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Optional;
-
-public interface PlayerRepository extends JpaRepository<Player, Long> {
-
-    Boolean existsById(String id);
-
-    Optional<Player> findById(String id);
+public interface PlayerRepository extends JpaRepository<Player, String> {
 }
