04d89f74df8ae0ee418eb6129e29655783b1fc0d
hothuuk <hothuuk@gmail.com>
2024-04-09 00:21:30 +0900

    create :: MonsterStatusCache

---
+++ src/main/java/com/htmake/htbot/discord/commands/battle/cache/MonsterStatusCache.java
@@ -0,0 +1,29 @@
+package com.htmake.htbot.discord.commands.battle.cache;
+
+import com.htmake.htbot.cache.CacheManager;
+import com.htmake.htbot.discord.commands.battle.data.MonsterStatus;
+
+public class MonsterStatusCache {
+
+    private final CacheManager<String, MonsterStatus> cache;
+
+    public MonsterStatusCache() {
+        this.cache = new CacheManager<>();
+    }
+
+    public void put(String key, MonsterStatus monsterStatus) {
+        cache.put(key, monsterStatus);
+    }
+
+    public MonsterStatus get(String key) {
+        return cache.get(key);
+    }
+
+    public boolean containsKey(String key) {
+        return cache.containsKey(key);
+    }
+
+    public void remove(String key) {
+        cache.remove(key);
+    }
+}
