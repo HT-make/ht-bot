404cdc9df1687d7ba9fc0e922c6ff7c33fe431e5
hothuuk <hothuuk@gmail.com>
2024-04-09 00:21:56 +0900

    create :: PlayerStatusCache

---
+++ src/main/java/com/htmake/htbot/discord/commands/battle/cache/PlayerStatusCache.java
@@ -0,0 +1,29 @@
+package com.htmake.htbot.discord.commands.battle.cache;
+
+import com.htmake.htbot.cache.CacheManager;
+import com.htmake.htbot.discord.commands.battle.data.PlayerStatus;
+
+public class PlayerStatusCache {
+
+    private final CacheManager<String, PlayerStatus> cache;
+
+    public PlayerStatusCache() {
+        this.cache = new CacheManager<>();
+    }
+
+    public void put(String key, PlayerStatus playerStatus) {
+        cache.put(key, playerStatus);
+    }
+
+    public PlayerStatus get(String key) {
+        return cache.get(key);
+    }
+
+    public boolean containsKey(String key) {
+        return cache.containsKey(key);
+    }
+
+    public void remove(String key) {
+        cache.remove(key);
+    }
+}
