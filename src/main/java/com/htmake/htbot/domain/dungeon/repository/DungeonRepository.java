ea3284c0dccc1a77ab4ff733733df6c46dad6ba5
hothuuk <hothuuk@gmail.com>
2024-04-09 00:16:05 +0900

    update :: Id 기본 타입 String으로 변경

--- src/main/java/com/htmake/htbot/domain/dungeon/repository/DungeonRepository.java
+++ src/main/java/com/htmake/htbot/domain/dungeon/repository/DungeonRepository.java
@@ -3,9 +3,5 @@ package com.htmake.htbot.domain.dungeon.repository;
 import com.htmake.htbot.domain.dungeon.entity.Dungeon;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Optional;
-
-public interface DungeonRepository extends JpaRepository<Dungeon, Long> {
-
-    Optional<Dungeon> findById(String id);
+public interface DungeonRepository extends JpaRepository<Dungeon, String> {
 }
