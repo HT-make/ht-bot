17ca0aa540770f02cc34a6e4eb52646b5ac99039
hothuuk <hothuuk@gmail.com>
2024-04-09 00:14:01 +0900

    update :: 클래스 구조 변경

--- src/main/java/com/htmake/htbot/discord/commands/battle/BattleCommand.java
+++ src/main/java/com/htmake/htbot/discord/commands/battle/BattleCommand.java
@@ -2,8 +2,6 @@ package com.htmake.htbot.discord.commands.battle;
 
 import com.htmake.htbot.discord.commands.battle.event.BattleAttackEvent;
 import com.htmake.htbot.discord.commands.battle.event.BattlePotionEvent;
-import com.htmake.htbot.unirest.HttpClient;
-import lombok.RequiredArgsConstructor;
 import net.dv8tion.jda.api.events.interaction.component.ButtonInteractionEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import org.jetbrains.annotations.NotNull;
@@ -12,18 +10,22 @@ import org.springframework.stereotype.Component;
 import java.util.List;
 
 @Component
-@RequiredArgsConstructor
 public class BattleCommand extends ListenerAdapter {
 
-    private final HttpClient httpClient;
+    private final BattleAttackEvent battleAttackEvent;
+
+    public BattleCommand() {
+        battleAttackEvent = new BattleAttackEvent();
+    }
 
     @Override
     public void onButtonInteraction(@NotNull ButtonInteractionEvent event) {
+
         List<String> components = List.of(event.getComponentId().split("-"));
         String component = components.get(0);
 
         if (component.equals("attack")) {
-            new BattleAttackEvent(event, httpClient);
+            battleAttackEvent.execute(event);
         } else if (component.equals("potion")) {
             new BattlePotionEvent(event, components.get(1));
         }
