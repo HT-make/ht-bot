5bb6498da03494ae4dda785f906f13beefd63ee9
오진서 <s22030@gsm.hs.kr>
2024-04-09 10:33:59 +0900

    create :: RandomShop Repositories

---
+++ src/main/java/com/htmake/htbot/domain/shop/repository/RandomShopArmorRepository.java
@@ -0,0 +1,7 @@
+package com.htmake.htbot.domain.shop.repository;
+
+import com.htmake.htbot.domain.shop.entity.RandomShopArmor;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RandomShopArmorRepository extends JpaRepository<RandomShopArmor, String> {
+}
