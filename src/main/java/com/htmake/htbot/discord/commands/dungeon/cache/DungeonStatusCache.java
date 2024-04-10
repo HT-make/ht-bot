f6b9fe34f6f944e75767254018520a752366d0b7
hothuuk <hothuuk@gmail.com>
2024-04-10 18:41:12 +0900

    create :: DungeonStatusCache 작성

---
+++ src/main/java/com/htmake/htbot/discord/commands/dungeon/cache/DungeonStatusCache.java
@@ -0,0 +1,29 @@
+package com.htmake.htbot.discord.commands.dungeon.cache;
+
+import com.htmake.htbot.cache.CacheManager;
+import com.htmake.htbot.discord.commands.dungeon.data.DungeonStatus;
+
+public class DungeonStatusCache {
+
+    private final CacheManager<String, DungeonStatus> cache;
+
+    public DungeonStatusCache() {
+        this.cache = new CacheManager<>();
+    }
+
+    public void put(String key, DungeonStatus DungeonStatus) {
+        cache.put(key, DungeonStatus);
+    }
+
+    public DungeonStatus get(String key) {
+        return cache.get(key);
+    }
+
+    public boolean containsKey(String key) {
+        return cache.containsKey(key);
+    }
+
+    public void remove(String key) {
+        cache.remove(key);
+    }
+}
