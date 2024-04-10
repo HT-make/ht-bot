f42e1f761b15db4a5354764b1b507b805b837a75
hothuuk <hothuuk@gmail.com>
2024-04-10 18:38:00 +0900

    add :: insertDropItem

--- src/main/java/com/htmake/htbot/domain/inventory/presentation/InventoryController.java
+++ src/main/java/com/htmake/htbot/domain/inventory/presentation/InventoryController.java
@@ -1,9 +1,12 @@
 package com.htmake.htbot.domain.inventory.presentation;
 
+import com.htmake.htbot.domain.inventory.presentation.data.request.InsertDropItemListRequest;
 import com.htmake.htbot.domain.inventory.presentation.data.request.InventoryInsertItemRequest;
 import com.htmake.htbot.domain.inventory.presentation.data.response.InventoryInfoListResponse;
+import com.htmake.htbot.domain.inventory.service.InsertDropItemService;
 import com.htmake.htbot.domain.inventory.service.InventoryInfoService;
 import com.htmake.htbot.domain.inventory.service.InventoryInsertItemService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -13,8 +16,10 @@ import org.springframework.web.bind.annotation.*;
 @RequiredArgsConstructor
 @RequestMapping("/inventory")
 public class InventoryController {
+
     private final InventoryInfoService inventoryInfoService;
     private final InventoryInsertItemService inventoryInsertItemService;
+    private final InsertDropItemService insertDropItemService;
 
     @GetMapping("/info/{player_id}")
     public ResponseEntity<InventoryInfoListResponse> getInventoryInfo(@PathVariable("player_id") String playerId) {
@@ -23,9 +28,20 @@ public class InventoryController {
     }
 
     @PostMapping("/insert/{player_id}")
-    public ResponseEntity<Void> insertInventory(@PathVariable("player_id") String playerId,
-                                                @RequestBody InventoryInsertItemRequest request) {
+    public ResponseEntity<Void> insertInventory(
+            @PathVariable("player_id") String playerId,
+            @RequestBody InventoryInsertItemRequest request
+    ) {
         inventoryInsertItemService.execute(playerId, request);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @PostMapping("/insert/dropItem/{player_id}")
+    public ResponseEntity<Void> insertDropItem(
+            @PathVariable("player_id") String playerId,
+            @RequestBody @Valid InsertDropItemListRequest request
+    ) {
+        insertDropItemService.execute(playerId, request);
         return new ResponseEntity<>(HttpStatus.OK);
     }
 }
