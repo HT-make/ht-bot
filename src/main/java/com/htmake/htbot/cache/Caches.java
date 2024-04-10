e72567250c79c7bdc1c0ddacf3c46a891efc6ef5
hothuuk <hothuuk@gmail.com>
2024-04-10 18:31:26 +0900

    add :: DungeonStatusCache 추가

--- src/main/java/com/htmake/htbot/cache/Caches.java
+++ src/main/java/com/htmake/htbot/cache/Caches.java
@@ -1,5 +1,6 @@
 package com.htmake.htbot.cache;
 
+import com.htmake.htbot.discord.commands.dungeon.cache.DungeonStatusCache;
 import com.htmake.htbot.discord.commands.battle.cache.MonsterStatusCache;
 import com.htmake.htbot.discord.commands.battle.cache.PlayerStatusCache;
 import com.htmake.htbot.discord.commands.battle.cache.SituationCache;
@@ -11,10 +12,12 @@ public class Caches {
     public static PlayerStatusCache playerStatusCache;
     public static MonsterStatusCache monsterStatusCache;
     public static SituationCache situationCache;
+    public static DungeonStatusCache dungeonStatusCache;
 
     public Caches() {
         playerStatusCache = new PlayerStatusCache();
         monsterStatusCache = new MonsterStatusCache();
         situationCache = new SituationCache();
+        dungeonStatusCache = new DungeonStatusCache();
     }
 }
