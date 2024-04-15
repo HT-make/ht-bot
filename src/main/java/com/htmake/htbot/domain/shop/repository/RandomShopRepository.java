3128594b576e514d55277b094ae6a1f3e8b8ce01
hothuuk <hothuuk@gmail.com>
2024-04-15 18:12:06 +0900

    add :: findByName

--- src/main/java/com/htmake/htbot/domain/shop/repository/RandomShopRepository.java
+++ src/main/java/com/htmake/htbot/domain/shop/repository/RandomShopRepository.java
@@ -3,5 +3,9 @@ package com.htmake.htbot.domain.shop.repository;
 import com.htmake.htbot.domain.shop.entity.RandomShop;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface RandomShopRepository extends JpaRepository<RandomShop, String> {
+
+    Optional<RandomShop> findByName(String name);
 }
