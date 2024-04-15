f809ad4b357d89fb7071ccaac83b9ce505318c1f
hothuuk <hothuuk@gmail.com>
2024-04-15 18:09:01 +0900

    add :: NOT_FOUND_RANDOM_SHOP, NOT_FOUND_PLAYER

--- src/main/java/com/htmake/htbot/global/error/ErrorCode.java
+++ src/main/java/com/htmake/htbot/global/error/ErrorCode.java
@@ -7,10 +7,15 @@ import lombok.Getter;
 @AllArgsConstructor
 public enum ErrorCode {
 
+    //PLAYER
+    NOT_FOUND_PLAYER("플레이어를 찾을 수 없습니다.", 404),
+
+    //SHOP
     NOT_ENOUGH_GOLD("골드가 충분하지 않습니다.", 400),
-    NOT_ENOUGH_QUANTITY("수량이 충분하지 않습니다.", 400),
-    NOT_FOUND_ITEM("해당 아이템을 찾을 수 없습니다.", 404)
-    ;
+    NOT_ENOUGH_QUANTITY("매진된 상품입니다.", 400),
+    NOT_FOUND_ITEM("해당 아이템을 찾을 수 없습니다.", 404),
+    NOT_FOUND_RANDOM_SHOP("랜덤 상점을 찾을 수 없습니다.", 404);
+
     private final String message;
     private final int status;
 }
