25264ba176533d890ff815307a2382d32e4770a1
hothuuk <hothuuk@gmail.com>
2024-04-15 18:14:03 +0900

    create :: SuccessPurchaseResponse 작성

---
+++ src/main/java/com/htmake/htbot/domain/shop/presentation/data/response/SuccessPurchaseResponse.java
@@ -0,0 +1,15 @@
+package com.htmake.htbot.domain.shop.presentation.data.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class SuccessPurchaseResponse {
+
+    private int gold;
+}
