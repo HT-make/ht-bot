c8b52c3fec622de83db71c85bb01c08b8315a6bf
hothuuk <hothuuk@gmail.com>
2024-04-15 11:29:42 +0900

    delete :: RandomShopArmor 관련 삭제

--- src/main/java/com/htmake/htbot/domain/shop/repository/RandomShopArmorRepository.java
+++
@@ -1,7 +0,0 @@
-package com.htmake.htbot.domain.shop.repository;
-
-import com.htmake.htbot.domain.shop.entity.RandomShopArmor;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RandomShopArmorRepository extends JpaRepository<RandomShopArmor, String> {
-}
