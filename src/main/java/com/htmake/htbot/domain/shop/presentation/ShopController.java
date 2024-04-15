38e006f10485f35f203dce0f522bf48dd739bb0a
오진서 <s22030@gsm.hs.kr>
2024-04-15 09:53:56 +0900

    add :: controller for randomShop purchase

--- src/main/java/com/htmake/htbot/domain/shop/presentation/ShopController.java
+++ src/main/java/com/htmake/htbot/domain/shop/presentation/ShopController.java
@@ -1,7 +1,10 @@
 package com.htmake.htbot.domain.shop.presentation;
 
+import com.htmake.htbot.domain.shop.presentation.data.request.RandomShopPurchaseRequest;
 import com.htmake.htbot.domain.shop.presentation.data.response.RandomShopItemListResponse;
+import com.htmake.htbot.domain.shop.service.RandomShopItemPurchaseService;
 import com.htmake.htbot.domain.shop.service.RandomShopItemService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -12,10 +15,20 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/shop")
 public class ShopController {
     private final RandomShopItemService randomShopItemService;
+    private final RandomShopItemPurchaseService randomShopItemPurchaseService;
 
     @GetMapping("/random/list")
     public ResponseEntity<RandomShopItemListResponse> randomShopItemListInfo() {
         RandomShopItemListResponse response = randomShopItemService.execute();
         return new ResponseEntity<>(response, HttpStatus.OK);
+    }
+
+    @PostMapping("/random/purchase/{player_id}")
+    public ResponseEntity<Void> randomShopPurchase(
+            @PathVariable("player_id") String playerId,
+            @RequestBody @Valid RandomShopPurchaseRequest request
+    ) {
+        randomShopItemPurchaseService.execute(playerId, request);
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 }
