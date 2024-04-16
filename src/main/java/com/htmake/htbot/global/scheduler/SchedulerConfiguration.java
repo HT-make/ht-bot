8775989dc53a67e88610fd05ba8182c15d792b35
hothuuk <hothuuk@gmail.com>
2024-04-16 09:50:12 +0900

    update :: @Scheduled 값 수정

--- src/main/java/com/htmake/htbot/global/scheduler/SchedulerConfiguration.java
+++ src/main/java/com/htmake/htbot/global/scheduler/SchedulerConfiguration.java
@@ -10,7 +10,7 @@ import org.springframework.stereotype.Component;
 public class SchedulerConfiguration {
     private final RandomShopItemShuffleService randomShopItemShuffleService;
 
-    @Scheduled(cron = "0 * * * *")
+    @Scheduled(cron = "0 0 * * * *")
     public void run() {
         randomShopItemShuffleService.execute();
     }
