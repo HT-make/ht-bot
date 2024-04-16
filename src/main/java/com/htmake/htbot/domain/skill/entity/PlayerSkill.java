64853eb18cf33a6b77ffae42cc1b3fef67d63cfc
hothuuk <hothuuk@gmail.com>
2024-04-16 11:26:02 +0900

    create :: PlayerSkill 작성

---
+++ src/main/java/com/htmake/htbot/domain/skill/entity/PlayerSkill.java
@@ -0,0 +1,26 @@
+package com.htmake.htbot.domain.skill.entity;
+
+import com.htmake.htbot.domain.player.entity.Player;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class PlayerSkill {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "player_skill_id")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "player_id")
+    private Player player;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "skill_id")
+    private Skill skill;
+}
