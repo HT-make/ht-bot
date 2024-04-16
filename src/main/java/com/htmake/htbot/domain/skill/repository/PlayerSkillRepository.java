8fb18c4569aa111d56fae53eacb268b100473566
hothuuk <hothuuk@gmail.com>
2024-04-17 00:47:37 +0900

    update :: findByNumberAndPlayer 추가

--- src/main/java/com/htmake/htbot/domain/skill/repository/PlayerSkillRepository.java
+++ src/main/java/com/htmake/htbot/domain/skill/repository/PlayerSkillRepository.java
@@ -2,14 +2,13 @@ package com.htmake.htbot.domain.skill.repository;
 
 import com.htmake.htbot.domain.player.entity.Player;
 import com.htmake.htbot.domain.skill.entity.PlayerSkill;
-import com.htmake.htbot.domain.skill.entity.Skill;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
 public interface PlayerSkillRepository extends JpaRepository<PlayerSkill, Long> {
 
-    boolean existsByPlayerAndSkill(Player player, Skill skill);
-
     List<PlayerSkill> findByPlayer(Player player);
+
+    PlayerSkill findByNumberAndPlayer(int number, Player player);
 }
