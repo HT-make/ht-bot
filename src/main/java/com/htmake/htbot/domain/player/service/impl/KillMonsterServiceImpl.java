2aa7bc482381e90629e3ef0448860871d78bb1a5
오진서 <s22030@gsm.hs.kr>
2024-04-17 18:09:01 +0900

    update :: using playerUtil for levelUp

--- src/main/java/com/htmake/htbot/domain/player/service/impl/KillMonsterServiceImpl.java
+++ src/main/java/com/htmake/htbot/domain/player/service/impl/KillMonsterServiceImpl.java
@@ -5,6 +5,7 @@ import com.htmake.htbot.domain.player.presentation.data.request.KillMonsterReque
 import com.htmake.htbot.domain.player.presentation.data.response.LevelUpResponse;
 import com.htmake.htbot.domain.player.repository.PlayerRepository;
 import com.htmake.htbot.domain.player.service.KillMonsterService;
+import com.htmake.htbot.global.util.PlayerUtil;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -15,6 +16,7 @@ import org.springframework.transaction.annotation.Transactional;
 public class KillMonsterServiceImpl implements KillMonsterService {
 
     private final PlayerRepository playerRepository;
+    private final PlayerUtil playerUtil;
 
     @Override
     public LevelUpResponse execute(String playerId, KillMonsterRequest request) {
@@ -22,25 +24,9 @@ public class KillMonsterServiceImpl implements KillMonsterService {
         Player player = playerRepository.findById(playerId)
                 .orElseThrow();
 
-        int currentExp = player.getCurrentExp() + request.getExp();
-        int gold = player.getGold() + request.getGold();
+        int exp = request.getExp();
+        int gold = request.getGold();
 
-        int maxExp = player.getMaxExp();
-        int level = player.getLevel();
-
-        boolean levelUp = false;
-
-        if (currentExp >= maxExp) {
-            currentExp -= maxExp;
-            maxExp *= level % 5 == 0 ? 2 : 1.3;
-            player.levelUp(maxExp);
-            levelUp = true;
-        }
-
-        player.killMonster(currentExp, gold);
-
-        playerRepository.save(player);
-
-        return LevelUpResponse.builder().levelUp(levelUp).build();
+        return playerUtil.executeLevelUp(player, exp, gold);
     }
 }
