3a49016846c1cbfa8ddb1a94f1d72218f9fb7b19
오진서 <s22030@gsm.hs.kr>
2024-04-17 18:17:31 +0900

    create :: quest monster quantity request

---
+++ src/main/java/com/htmake/htbot/domain/quest/presentation/data/request/PlayerQuestMonsterQuantityRequest.java
@@ -0,0 +1,14 @@
+package com.htmake.htbot.domain.quest.presentation.data.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class PlayerQuestMonsterQuantityRequest {
+    String name;
+}
