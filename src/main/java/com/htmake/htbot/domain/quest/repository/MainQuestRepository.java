f766401c9b7c442911c1841dc8f73d5a50d98b05
오진서 <s22030@gsm.hs.kr>
2024-04-17 18:13:09 +0900

    create :: quest repositories

---
+++ src/main/java/com/htmake/htbot/domain/quest/repository/MainQuestRepository.java
@@ -0,0 +1,7 @@
+package com.htmake.htbot.domain.quest.repository;
+
+import com.htmake.htbot.domain.quest.entity.MainQuest;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MainQuestRepository extends JpaRepository<MainQuest, Long> {
+}
