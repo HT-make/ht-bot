f3555194ea0bcb7f0eace04f0e2daaf314e632ad
오진서 <s22030@gsm.hs.kr>
2024-04-17 18:18:25 +0900

    create :: quest controller

---
+++ src/main/java/com/htmake/htbot/domain/quest/presentation/QuestController.java
@@ -0,0 +1,43 @@
+package com.htmake.htbot.domain.quest.presentation;
+
+import com.htmake.htbot.domain.quest.presentation.data.request.PlayerQuestMonsterQuantityRequest;
+import com.htmake.htbot.domain.quest.presentation.data.response.PlayerQuestInfoResponse;
+import com.htmake.htbot.domain.quest.service.PlayerQuestInfoService;
+import com.htmake.htbot.domain.quest.service.PlayerQuestMonsterQuantityService;
+import com.htmake.htbot.domain.quest.service.PlayerQuestProgressService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/quest")
+public class QuestController {
+
+    private final PlayerQuestInfoService playerQuestInfoService;
+    private final PlayerQuestProgressService playerQuestProgressService;
+    private final PlayerQuestMonsterQuantityService playerQuestMonsterQuantityService;
+
+    @GetMapping("/{player_id}")
+    public ResponseEntity<PlayerQuestInfoResponse> quest(@PathVariable("player_id") String playerId) {
+        PlayerQuestInfoResponse response = playerQuestInfoService.execute(playerId);
+        return new ResponseEntity<>(response, HttpStatus.OK);
+    }
+
+    @PatchMapping("/progress/{player_id}")
+    public ResponseEntity<Void> questProgress(@PathVariable("player_id") String playerId) {
+        playerQuestProgressService.execute(playerId);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @PatchMapping("/monster/{player_id}")
+    public ResponseEntity<Void> questMonster(
+            @PathVariable("player_id") String playerId,
+            @RequestBody @Valid PlayerQuestMonsterQuantityRequest request
+    ) {
+        playerQuestMonsterQuantityService.execute(playerId, request);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+}
