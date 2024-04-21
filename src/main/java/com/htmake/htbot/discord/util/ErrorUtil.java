e9370a6e6548ad352e89d126f739340bc6381134
hothuuk <hothuuk@gmail.com>
2024-04-21 17:26:10 +0900

    create :: ErrorUtil 작성

---
+++ src/main/java/com/htmake/htbot/discord/util/ErrorUtil.java
@@ -0,0 +1,31 @@
+package com.htmake.htbot.discord.util;
+
+import net.dv8tion.jda.api.EmbedBuilder;
+import net.dv8tion.jda.api.entities.Message;
+import net.dv8tion.jda.api.entities.MessageEmbed;
+import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
+
+import java.awt.*;
+import java.util.Collections;
+
+public class ErrorUtil {
+
+    public void sendError(SlashCommandInteractionEvent event, String title, String description) {
+        MessageEmbed embed = buildEmbed(title, description);
+        event.replyEmbeds(embed).queue();
+    }
+
+    public void sendError(Message message, String title, String description) {
+        MessageEmbed embed = buildEmbed(title, description);
+        message.editMessageComponents(Collections.emptyList()).queue();
+        message.replyEmbeds(embed).queue();
+    }
+
+    private MessageEmbed buildEmbed(String title, String description) {
+        return new EmbedBuilder()
+                .setColor(Color.ORANGE)
+                .setTitle(":warning: " + title)
+                .setDescription(description)
+                .build();
+    }
+}
