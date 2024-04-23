726266e024986d0c11dc5f1e8b71c9a709ad79aa
hothuuk <hothuuk@gmail.com>
2024-04-23 14:18:15 +0900

    update :: class 명 변경

--- src/main/java/com/htmake/htbot/discord/commands/battle/BattleCommand.java
+++ src/main/java/com/htmake/htbot/discord/commands/battle/BattleCommand.java
@@ -1,6 +1,6 @@
 package com.htmake.htbot.discord.commands.battle;
 
-import com.htmake.htbot.discord.commands.battle.event.PlayerAttackEvent;
+import com.htmake.htbot.discord.commands.battle.event.PlayerAttackButtonEvent;
 import com.htmake.htbot.discord.commands.battle.event.BattlePotionEvent;
 import net.dv8tion.jda.api.events.interaction.component.ButtonInteractionEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
@@ -12,10 +12,10 @@ import java.util.List;
 @Component
 public class BattleCommand extends ListenerAdapter {
 
-    private final PlayerAttackEvent playerAttackEvent;
+    private final PlayerAttackButtonEvent playerAttackButtonEvent;
 
     public BattleCommand() {
-        playerAttackEvent = new PlayerAttackEvent();
+        this.playerAttackButtonEvent = new PlayerAttackButtonEvent();
     }
 
     @Override
@@ -24,7 +24,7 @@ public class BattleCommand extends ListenerAdapter {
 
         if (componentList.get(0).equals("battle")) {
             if (componentList.get(1).equals("attack")) {
-                playerAttackEvent.execute(event);
+                playerAttackButtonEvent.execute(event);
             } else if (componentList.get(1).equals("potion")) {
                 new BattlePotionEvent(event, componentList.get(2));
             }
