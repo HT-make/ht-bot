d595d122248c3aa784606219a0491d5029edd5d9
hothuuk <hothuuk@gmail.com>
2024-04-24 23:58:18 +0900

    update :: handleDungeonInfo -> DungeonEntrySlashEvent로 분리

--- src/main/java/com/htmake/htbot/discord/commands/dungeon/DungeonCommand.java
+++ src/main/java/com/htmake/htbot/discord/commands/dungeon/DungeonCommand.java
@@ -1,37 +1,35 @@
 package com.htmake.htbot.discord.commands.dungeon;
 
-import com.htmake.htbot.discord.commands.dungeon.event.DungeonCloseEvent;
-import com.htmake.htbot.discord.commands.dungeon.event.DungeonEntryEvent;
-import com.htmake.htbot.discord.commands.dungeon.event.NextDungeonEntryEvent;
-import lombok.extern.slf4j.Slf4j;
-import net.dv8tion.jda.api.EmbedBuilder;
-import net.dv8tion.jda.api.entities.MessageEmbed;
+import com.htmake.htbot.discord.commands.dungeon.event.DungeonCloseButtonEvent;
+import com.htmake.htbot.discord.commands.dungeon.event.DungeonEntrySelectEvent;
+import com.htmake.htbot.discord.commands.dungeon.event.DungeonEntrySlashEvent;
+import com.htmake.htbot.discord.commands.dungeon.event.NextDungeonEntryButtonEvent;
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
 import net.dv8tion.jda.api.events.interaction.component.ButtonInteractionEvent;
 import net.dv8tion.jda.api.events.interaction.component.StringSelectInteractionEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
-import net.dv8tion.jda.api.interactions.components.buttons.Button;
-import net.dv8tion.jda.api.interactions.components.selections.SelectOption;
-import net.dv8tion.jda.api.interactions.components.selections.StringSelectMenu;
 import org.jetbrains.annotations.NotNull;
 import org.springframework.stereotype.Component;
 
-import java.awt.*;
-import java.util.*;
 import java.util.List;
 
 @Component
-@Slf4j
 public class DungeonCommand extends ListenerAdapter {
 
-    private final DungeonEntryEvent dungeonEntryEvent;
-    private final NextDungeonEntryEvent nextDungeonEntryEvent;
-    private final DungeonCloseEvent dungeonCloseEvent;
+    private final DungeonEntrySlashEvent dungeonEntrySlashEvent;
+    private final DungeonEntrySelectEvent dungeonEntrySelectEvent;
+
+    private final NextDungeonEntryButtonEvent nextDungeonEntryButtonEvent;
+
+    private final DungeonCloseButtonEvent dungeonCloseButtonEvent;
 
     public DungeonCommand() {
-        this.dungeonEntryEvent = new DungeonEntryEvent();
-        this.nextDungeonEntryEvent = new NextDungeonEntryEvent();
-        this.dungeonCloseEvent = new DungeonCloseEvent();
+        this.dungeonEntrySlashEvent = new DungeonEntrySlashEvent();
+        this.dungeonEntrySelectEvent = new DungeonEntrySelectEvent();
+
+        this.nextDungeonEntryButtonEvent = new NextDungeonEntryButtonEvent();
+
+        this.dungeonCloseButtonEvent = new DungeonCloseButtonEvent();
     }
 
     @Override
@@ -39,43 +37,8 @@ public class DungeonCommand extends ListenerAdapter {
         String command = event.getName();
 
         if (command.equals("던전-입장")) {
-            handleDungeonInfo(event);
+            dungeonEntrySlashEvent.execute(event);
         }
-    }
-
-    private void handleDungeonInfo(SlashCommandInteractionEvent event) {
-
-        MessageEmbed embed = new EmbedBuilder()
-                .setColor(Color.GREEN)
-                .setAuthor(event.getUser().getName())
-                .setTitle(":key:던전")
-                .setDescription(
-                        """
-                        :warning: 주의 :warning:
-                        
-                        전투에서 패배 시 해당 던전에서 얻은
-                        아이템을 모두 잃으니 주의하세요!
-                        
-                        전투 중 후퇴 시에도 패배로 처리됩니다.
-                        """)
-                .build();
-
-        StringSelectMenu menu = StringSelectMenu.create("dungeonMenu")
-                .setPlaceholder("던전 선택")
-                .addOptions(Arrays.asList(
-                        SelectOption.of("드넓은 초원 | 권장 레벨 1~10", "dungeon-enter-dungeon1"),
-                        SelectOption.of("깊은 동굴 | 권장 레벨 10~20", "dungeon-enter-dungeon2"),
-                        SelectOption.of("끈적이는 늪 | 권장 레벨 20~30", "dungeon-enter-dungeon3"),
-                        SelectOption.of("어두운 숲 | 권장 레벨 30~40", "dungeon-enter-dungeon4"),
-                        SelectOption.of("몰락한 성 | 권장 레벨 40~50", "dungeon-enter-dungeon5"),
-                        SelectOption.of("용암 지대 | 권장 레벨 50~60", "dungeon-enter-dungeon6")
-                ))
-                .build();
-
-        event.replyEmbeds(embed)
-                .addActionRow(menu)
-                .addActionRow(Button.danger("cancel", "닫기"))
-                .queue();
     }
 
     @Override
@@ -84,7 +47,7 @@ public class DungeonCommand extends ListenerAdapter {
 
         if (componentList.get(0).equals("dungeon")) {
             if (componentList.get(1).equals("enter")) {
-                dungeonEntryEvent.execute(event, componentList.get(2));
+                dungeonEntrySelectEvent.execute(event, componentList.get(2));
             }
         }
     }
@@ -95,9 +58,9 @@ public class DungeonCommand extends ListenerAdapter {
 
         if (componentList.get(0).equals("dungeon")) {
             if (componentList.get(1).equals("next")) {
-                nextDungeonEntryEvent.execute(event);
+                nextDungeonEntryButtonEvent.execute(event);
             } else if (componentList.get(1).equals("close")) {
-                dungeonCloseEvent.execute(event);
+                dungeonCloseButtonEvent.execute(event);
             }
         }
     }
