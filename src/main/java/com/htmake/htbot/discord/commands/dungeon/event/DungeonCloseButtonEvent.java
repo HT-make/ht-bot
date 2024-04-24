9327d590846c56448567ec69ba8edba0efd89d59
hothuuk <hothuuk@gmail.com>
2024-04-25 00:00:03 +0900

    update :: 클래스명 변경 및 간단한 코드 수정

--- src/main/java/com/htmake/htbot/discord/commands/dungeon/event/DungeonCloseButtonEvent.java
+++ src/main/java/com/htmake/htbot/discord/commands/dungeon/event/DungeonCloseButtonEvent.java
@@ -1,8 +1,7 @@
 package com.htmake.htbot.discord.commands.dungeon.event;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.htmake.htbot.discord.util.ErrorUtil;
+import com.htmake.htbot.discord.util.ObjectMapperUtil;
 import com.htmake.htbot.global.cache.Caches;
 import com.htmake.htbot.discord.commands.dungeon.cache.DungeonStatusCache;
 import com.htmake.htbot.discord.commands.dungeon.data.DungeonStatus;
@@ -12,9 +11,7 @@ import com.htmake.htbot.global.unirest.impl.HttpClientImpl;
 import com.mashape.unirest.http.HttpResponse;
 import com.mashape.unirest.http.JsonNode;
 import kotlin.Pair;
-import lombok.extern.slf4j.Slf4j;
 import net.dv8tion.jda.api.EmbedBuilder;
-import net.dv8tion.jda.api.entities.Message;
 import net.dv8tion.jda.api.entities.MessageEmbed;
 import net.dv8tion.jda.api.events.interaction.component.ButtonInteractionEvent;
 
@@ -24,17 +21,18 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-@Slf4j
-public class DungeonCloseEvent {
+public class DungeonCloseButtonEvent {
 
     private final HttpClient httpClient;
     private final ErrorUtil errorUtil;
+    private final ObjectMapperUtil objectMapperUtil;
 
     private final DungeonStatusCache dungeonStatusCache;
 
-    public DungeonCloseEvent() {
+    public DungeonCloseButtonEvent() {
         this.httpClient = new HttpClientImpl();
         this.errorUtil = new ErrorUtil();
+        this.objectMapperUtil = new ObjectMapperUtil();
 
         this.dungeonStatusCache = Caches.dungeonStatusCache;
     }
@@ -46,10 +44,10 @@ public class DungeonCloseEvent {
         List<GetItem> getItemList = dungeonStatus.getGetItemList();
 
         if (!getItemList.isEmpty())  {
-            HttpResponse<JsonNode> response = insertItemRequest(getItemList, playerId);
+            HttpResponse<JsonNode> response = request(getItemList, playerId);
 
             if (response.getStatus() == 200) {
-                handleSuccessResponse(event);
+                requestSuccess(event);
             } else {
                 errorUtil.sendError(event.getMessage(), "던전 퇴장", "던전 퇴장에 실패하였습니다.");
             }
@@ -58,35 +56,25 @@ public class DungeonCloseEvent {
         dungeonStatusCache.remove(playerId);
     }
 
-    private HttpResponse<JsonNode> insertItemRequest(List<GetItem> getItemList, String playerId) {
-        ObjectMapper objectMapper = new ObjectMapper();
+    private HttpResponse<JsonNode> request(List<GetItem> getItemList, String playerId) {
         Map<String, Object> requestData = new HashMap<>();
-
         requestData.put("dropItemList", getItemList);
 
         String endPoint = "/inventory/insert/dropItem/{player_id}";
         Pair<String, String> routeParam = new Pair<>("player_id", playerId);
-        String requestBody;
-
-        try {
-            requestBody = objectMapper.writeValueAsString(requestData);
-        } catch (JsonProcessingException e) {
-            throw new RuntimeException(e);
-        }
+        String requestBody = objectMapperUtil.mapper(requestData);
 
         return httpClient.sendPostRequest(endPoint, routeParam, requestBody);
     }
 
-    private void handleSuccessResponse(ButtonInteractionEvent event) {
-        Message message = event.getMessage();
-
+    private void requestSuccess(ButtonInteractionEvent event) {
         MessageEmbed embed = new EmbedBuilder()
                 .setColor(Color.GREEN)
                 .setTitle("던전 퇴장")
                 .setDescription("던전에서 퇴장하였습니다.")
                 .build();
 
-        message.editMessageComponents(Collections.emptyList()).queue();
-        message.editMessageEmbeds(embed).queue();
+        event.getMessage().editMessageComponents(Collections.emptyList()).queue();
+        event.getMessage().editMessageEmbeds(embed).queue();
     }
 }
