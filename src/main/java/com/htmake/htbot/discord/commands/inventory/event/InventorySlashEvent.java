b41a43f3fad5b3aad76751566adad688bac33db5
오진서 <s22030@gsm.hs.kr>
2024-04-25 18:26:20 +0900

    add :: added inventory embed and refactoring

--- src/main/java/com/htmake/htbot/discord/commands/inventory/event/InventorySlashEvent.java
+++ src/main/java/com/htmake/htbot/discord/commands/inventory/event/InventorySlashEvent.java
@@ -1,6 +1,11 @@
 package com.htmake.htbot.discord.commands.inventory.event;
 
+import com.htmake.htbot.discord.commands.inventory.cache.InventoryCache;
+import com.htmake.htbot.discord.commands.inventory.data.InventoryItem;
+import com.htmake.htbot.discord.commands.inventory.data.Inventory;
+import com.htmake.htbot.discord.commands.inventory.util.InventoryUtil;
 import com.htmake.htbot.discord.util.ErrorUtil;
+import com.htmake.htbot.global.cache.CacheFactory;
 import com.htmake.htbot.global.unirest.HttpClient;
 import com.htmake.htbot.global.unirest.impl.HttpClientImpl;
 import com.mashape.unirest.http.HttpResponse;
@@ -11,18 +16,29 @@ import net.dv8tion.jda.api.entities.MessageEmbed;
 import net.dv8tion.jda.api.entities.User;
 import net.dv8tion.jda.api.interactions.components.buttons.Button;
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
+import net.dv8tion.jda.api.interactions.components.selections.SelectOption;
+import net.dv8tion.jda.api.interactions.components.selections.StringSelectMenu;
 import org.json.JSONArray;
 import org.json.JSONObject;
 
 import java.awt.*;
+import java.util.Arrays;
+import java.util.List;
+
 public class InventorySlashEvent {
 
     private final HttpClient httpClient;
     private final ErrorUtil errorUtil;
+    private final InventoryUtil inventoryUtil;
+
+    private final InventoryCache inventoryCache;
 
     public InventorySlashEvent() {
         this.httpClient = new HttpClientImpl();
         this.errorUtil = new ErrorUtil();
+        this.inventoryUtil = new InventoryUtil();
+
+        this.inventoryCache = CacheFactory.inventoryCache;
     }
 
     public void execute(SlashCommandInteractionEvent event) {
@@ -44,36 +60,70 @@ public class InventorySlashEvent {
     }
 
     private void requestSuccess(SlashCommandInteractionEvent event, JSONObject inventoryObject) {
-        JSONArray inventoryArray = inventoryObject.getJSONArray("inventoryList");
-        MessageEmbed embed = buildEmbed(event.getUser(), inventoryArray);
+        JSONArray inventoryItemArray = inventoryObject.getJSONArray("inventoryList");
+        List<InventoryItem> inventoryItemList = inventoryUtil.toInventoryItemList(inventoryItemArray);
+
+        Inventory inventory = toInventory(inventoryObject, inventoryItemList);
+
+        inventoryCache.put(event.getUser().getId(), inventory);
+
+        MessageEmbed embed = buildEmbed(event.getUser(), inventory);
+
+        StringSelectMenu menu = buildMenu();
 
         event.replyEmbeds(embed)
                 .addActionRow(
                         Button.primary("inventory-left-0", "◄").asDisabled(),
                         Button.secondary("blank", "1/5").asDisabled(),
-                        Button.primary("inventory-right-2", "►")
+                        Button.primary("inventory-right-2", "►"),
+                        Button.danger("cancel", "닫기")
                 )
+                .addActionRow(menu)
                 .queue();
     }
 
-    private MessageEmbed buildEmbed(User user, JSONArray inventoryArray) {
+    private Inventory toInventory(JSONObject inventoryObject, List<InventoryItem> inventoryItemList) {
+        return Inventory.builder()
+                .gold(inventoryObject.getInt("gold"))
+                .gem(inventoryObject.getInt("gem"))
+                .category("all")
+                .inventoryItemList(inventoryItemList)
+                .build();
+    }
+
+    private MessageEmbed buildEmbed(User user, Inventory inventory) {
         String profileUrl = user.getAvatarUrl() != null ? user.getAvatarUrl() : user.getDefaultAvatarUrl();
 
         EmbedBuilder embedBuilder = new EmbedBuilder()
                 .setColor(Color.GREEN)
                 .setAuthor(user.getName(), null, profileUrl)
-                .setTitle(":backpack: 인벤토리");
+                .setTitle(":backpack: 인벤토리")
+                .addField(":coin: 골드 ", "" + inventory.getGold(), true)
+                .addField(":gem: 젬 ", "" + inventory.getGem(), true)
+                .addBlankField(true)
+                .addBlankField(false);
+
+        List<InventoryItem> inventoryItemList = inventory.getInventoryItemList();
 
         for (int i = 0; i < 9; i++) {
-            if (i >= inventoryArray.length()) break;
+            if (i >= inventoryItemList.size()) break;
 
-            JSONObject item = inventoryArray.getJSONObject(i);
-            String itemName = item.getString("name");
-            int itemQuantity = item.getInt("quantity");
-
-            embedBuilder.addField(itemName, "수량: " + itemQuantity, true);
+            InventoryItem inventoryItem = inventoryItemList.get(i);
+            embedBuilder.addField(inventoryItem.getName(), "수량: " + inventoryItem.getQuantity(), true);
         }
 
         return embedBuilder.build();
+    }
+
+    private StringSelectMenu buildMenu() {
+        return StringSelectMenu.create("inventoryMenu")
+                .setPlaceholder("현재 페이지 : 전체")
+                .addOptions(Arrays.asList(
+                        SelectOption.of("전체", "inventory-category-all"),
+                        SelectOption.of("장비", "inventory-category-equipment"),
+                        SelectOption.of("치장", "inventory-category-accessory"),
+                        SelectOption.of("기타", "inventory-category-misc")
+                ))
+                .build();
     }
 }
