a08e4ea748ab6165bde79251ac73e3577e8ec5de
오진서 <s22030@gsm.hs.kr>
2024-04-30 15:47:17 +0900

    add :: HttpClient requestParamList

--- src/main/java/com/htmake/htbot/global/unirest/HttpClient.java
+++ src/main/java/com/htmake/htbot/global/unirest/HttpClient.java
@@ -5,6 +5,8 @@ import com.mashape.unirest.http.JsonNode;
 import kotlin.Pair;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
+
 @Component
 public interface HttpClient {
 
@@ -12,7 +14,6 @@ public interface HttpClient {
     HttpResponse<JsonNode> sendPostRequest(String endPoint, Pair<String, String> routeParam, String requestBody);
     HttpResponse<JsonNode> sendGetRequest(String endPoint);
     HttpResponse<JsonNode> sendGetRequest(String endPoint, Pair<String, String> routeParam);
+    HttpResponse<JsonNode> sendGetRequest(String endPoint,  List<Pair<String, String>> requestParamList);
     HttpResponse<JsonNode> sendPatchRequest(String endPoint, Pair<String, String> routeParam, String requestBody);
-    HttpResponse<JsonNode> sendPatchRequest(String endPoint, Pair<String, String> routeParam);
 }
-
