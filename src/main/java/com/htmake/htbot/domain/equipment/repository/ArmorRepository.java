7a0f93a9564f2aeacda4fbd53a8b6e47514004e2
오진서 <s22030@gsm.hs.kr>
2024-04-30 15:41:28 +0900

    add :: findByName

--- src/main/java/com/htmake/htbot/domain/equipment/repository/ArmorRepository.java
+++ src/main/java/com/htmake/htbot/domain/equipment/repository/ArmorRepository.java
@@ -3,5 +3,8 @@ package com.htmake.htbot.domain.equipment.repository;
 import com.htmake.htbot.domain.equipment.entity.Armor;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface ArmorRepository extends JpaRepository<Armor, String> {
+    Optional<Armor> findByName(String name);
 }
--- src/main/java/com/htmake/htbot/domain/equipment/repository/WeaponRepository.java
+++ src/main/java/com/htmake/htbot/domain/equipment/repository/WeaponRepository.java
@@ -3,5 +3,8 @@ package com.htmake.htbot.domain.equipment.repository;
 import com.htmake.htbot.domain.equipment.entity.Weapon;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface WeaponRepository extends JpaRepository<Weapon, String> {
+    Optional<Weapon> findByName(String name);
 }
