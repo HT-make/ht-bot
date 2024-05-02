545d447820c1158e39642c2fa00e0ab198db5c57
hothuuk <kijkj123@gmail.com>
2024-05-03 00:05:17 +0900

    update :: modified to suit changed structure

--- src/main/java/com/htmake/htbot/discord/commands/dungeon/event/fieldDungeon/NextStageEntryButtonEvent.java
+++ src/main/java/com/htmake/htbot/discord/commands/dungeon/event/fieldDungeon/NextStageEntryButtonEvent.java
@@ -1,9 +1,9 @@
-package com.htmake.htbot.discord.commands.dungeon.event;
+package com.htmake.htbot.discord.commands.dungeon.event.fieldDungeon;
 
-import com.htmake.htbot.discord.commands.dungeon.cache.DungeonStatusCache;
+import com.htmake.htbot.discord.commands.dungeon.cache.FieldDungeonStatusCache;
 import com.htmake.htbot.discord.commands.dungeon.data.DungeonMonster;
 import com.htmake.htbot.discord.commands.dungeon.data.DungeonPlayer;
-import com.htmake.htbot.discord.commands.dungeon.data.DungeonStatus;
+import com.htmake.htbot.discord.commands.dungeon.data.FieldDungeonStatus;
 import com.htmake.htbot.discord.commands.dungeon.util.DungeonUtil;
 import com.htmake.htbot.discord.util.ErrorUtil;
 import com.htmake.htbot.global.cache.CacheFactory;
@@ -11,40 +11,40 @@ import net.dv8tion.jda.api.entities.MessageEmbed;
 import net.dv8tion.jda.api.events.interaction.component.ButtonInteractionEvent;
 import net.dv8tion.jda.api.interactions.components.buttons.Button;
 
-import java.util.List;
+import java.util.Map;
 
-public class NextDungeonEntryButtonEvent {
+public class NextStageEntryButtonEvent {
 
     private final ErrorUtil errorUtil;
     private final DungeonUtil dungeonUtil;
 
-    private final DungeonStatusCache dungeonStatusCache;
+    private final FieldDungeonStatusCache fieldDungeonStatusCache;
 
-    public NextDungeonEntryButtonEvent() {
+    public NextStageEntryButtonEvent() {
         this.errorUtil = new ErrorUtil();
         this.dungeonUtil = new DungeonUtil();
 
-        this.dungeonStatusCache = CacheFactory.dungeonStatusCache;
+        this.fieldDungeonStatusCache = CacheFactory.fieldDungeonStatusCache;
     }
 
     public void execute(ButtonInteractionEvent event) {
         String playerId = event.getUser().getId();
 
-        if (!dungeonStatusCache.containsKey(playerId)) {
+        if (!fieldDungeonStatusCache.containsKey(playerId)) {
             errorUtil.sendError(event.getHook(), "던전 입장", "던전 입장에 실패했습니다.");
             return;
         }
 
-        DungeonStatus dungeonStatus = dungeonStatusCache.get(playerId);
+        FieldDungeonStatus fieldDungeonStatus = fieldDungeonStatusCache.get(playerId);
 
-        int stage = dungeonStatus.getStage() + 1;
-        stageUp(playerId, dungeonStatus, stage);
+        int stage = fieldDungeonStatus.getStage() + 1;
+        stageUp(playerId, fieldDungeonStatus, stage);
 
-        List<DungeonMonster> dungeonMonsterList = dungeonStatus.getDungeonMonsterList();
+        Map<Integer, DungeonMonster> dungeonMonsterList = fieldDungeonStatus.getMonsterByStage();
 
-        String dungeonName = dungeonStatus.getName();
-        DungeonMonster dungeonMonster = dungeonUtil.randomMonster(dungeonMonsterList, stage);
-        DungeonPlayer dungeonPlayer = dungeonStatus.getDungeonPlayer();
+        String dungeonName = fieldDungeonStatus.getName();
+        DungeonMonster dungeonMonster = dungeonMonsterList.get(stage);
+        DungeonPlayer dungeonPlayer = fieldDungeonStatus.getDungeonPlayer();
 
         dungeonUtil.savePlayerStatus(playerId, dungeonPlayer);
         dungeonUtil.saveMonsterStatus(playerId, dungeonMonster);
@@ -64,8 +64,8 @@ public class NextDungeonEntryButtonEvent {
                 .queue();
     }
 
-    private void stageUp(String playerId, DungeonStatus dungeonStatus, int stage) {
-        dungeonStatus.setStage(stage);
-        dungeonStatusCache.put(playerId, dungeonStatus);
+    private void stageUp(String playerId, FieldDungeonStatus fieldDungeonStatus, int stage) {
+        fieldDungeonStatus.setStage(stage);
+        fieldDungeonStatusCache.put(playerId, fieldDungeonStatus);
     }
 }
