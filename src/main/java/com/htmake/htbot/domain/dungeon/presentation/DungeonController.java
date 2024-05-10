f5bfc91aa48614e885651021bfd75d626fb62f13
hothuuk <kijkj123@gmail.com>
2024-05-10 10:02:18 +0900

    add :: boss dungeon services

--- src/main/java/com/htmake/htbot/domain/dungeon/presentation/DungeonController.java
+++ src/main/java/com/htmake/htbot/domain/dungeon/presentation/DungeonController.java
@@ -1,14 +1,14 @@
 package com.htmake.htbot.domain.dungeon.presentation;
 
+import com.htmake.htbot.domain.dungeon.presentation.data.response.BossDungeonInfoResponse;
 import com.htmake.htbot.domain.dungeon.presentation.data.response.DungeonResponse;
+import com.htmake.htbot.domain.dungeon.service.BossDungeonEntryService;
+import com.htmake.htbot.domain.dungeon.service.BossDungeonInfoService;
 import com.htmake.htbot.domain.dungeon.service.FieldDungeonEntryService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -16,10 +16,30 @@ import org.springframework.web.bind.annotation.RestController;
 public class DungeonController {
 
     private final FieldDungeonEntryService fieldDungeonEntryService;
+    private final BossDungeonInfoService bossDungeonInfoService;
+    private final BossDungeonEntryService bossDungeonEntryService;
 
     @GetMapping("/{dungeon_id}")
-    public ResponseEntity<DungeonResponse> findById(@PathVariable("dungeon_id") String dungeonId) {
+    public ResponseEntity<DungeonResponse> fieldDungeonEntry(@PathVariable("dungeon_id") String dungeonId) {
         DungeonResponse response = fieldDungeonEntryService.execute(dungeonId);
+        return new ResponseEntity<>(response, HttpStatus.OK);
+    }
+
+    @GetMapping("/{dungeon_id}/{player_id}")
+    public ResponseEntity<BossDungeonInfoResponse> bossDungeonInfo(
+            @PathVariable("dungeon_id") String dungeonId,
+            @PathVariable("player_id") String playerId
+    ) {
+        BossDungeonInfoResponse response = bossDungeonInfoService.execute(dungeonId, playerId);
+        return new ResponseEntity<>(response, HttpStatus.OK);
+    }
+
+    @PatchMapping("/{dungeon_id}/{player_id}")
+    public ResponseEntity<DungeonResponse> bossDungeonEntry(
+            @PathVariable("dungeon_id") String dungeonId,
+            @PathVariable("player_id") String playerId
+    ) {
+        DungeonResponse response = bossDungeonEntryService.execute(dungeonId, playerId);
         return new ResponseEntity<>(response, HttpStatus.OK);
     }
 }
