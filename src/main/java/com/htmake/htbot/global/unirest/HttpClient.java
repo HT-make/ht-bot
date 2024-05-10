dcf57cd5e4ba2c49c2ff90ad2a6338786063fcce
hothuuk <kijkj123@gmail.com>
2024-05-10 10:11:13 +0900

    add :: 두 개의 루트 파람 값을 가진 request 추가

--- src/main/java/com/htmake/htbot/global/unirest/HttpClient.java
+++ src/main/java/com/htmake/htbot/global/unirest/HttpClient.java
@@ -11,9 +11,18 @@ import java.util.List;
 public interface HttpClient {
 
     HttpResponse<JsonNode> sendPostRequest(String endPoint, String requestBody);
+
     HttpResponse<JsonNode> sendPostRequest(String endPoint, Pair<String, String> routeParam, String requestBody);
+
     HttpResponse<JsonNode> sendGetRequest(String endPoint);
+
     HttpResponse<JsonNode> sendGetRequest(String endPoint, Pair<String, String> routeParam);
-    HttpResponse<JsonNode> sendGetRequest(String endPoint,  List<Pair<String, String>> requestParamList);
+
+    HttpResponse<JsonNode> sendGetRequest(String endPoint, Pair<String, String> firstRouteParam, Pair<String, String> secondRouteParam);
+
+    HttpResponse<JsonNode> sendGetRequest(String endPoint, List<Pair<String, String>> requestParamList);
+
     HttpResponse<JsonNode> sendPatchRequest(String endPoint, Pair<String, String> routeParam, String requestBody);
+
+    HttpResponse<JsonNode> sendPatchRequest(String endPoint, Pair<String, String> firstRouteParam, Pair<String, String> secondRouteParam);
 }
