5fcc8f819c1d03b4ba6476900b9e241a956ab775
hothuuk <kijkj123@gmail.com>
2024-05-10 10:12:16 +0900

    update :: dungeonType에 맞는 결과 액션을 취하도록 변경

--- src/main/java/com/htmake/htbot/discord/commands/battle/action/MonsterKillAction.java
+++ src/main/java/com/htmake/htbot/discord/commands/battle/action/MonsterKillAction.java
@@ -3,48 +3,66 @@ package com.htmake.htbot.discord.commands.battle.action;
 import com.htmake.htbot.discord.commands.battle.data.MonsterStatus;
 import com.htmake.htbot.discord.commands.battle.data.PlayerStatus;
 import com.htmake.htbot.discord.commands.battle.util.BattleUtil;
+import com.htmake.htbot.discord.commands.dungeon.cache.DungeonTypeCache;
+import com.htmake.htbot.discord.commands.dungeon.enums.DungeonType;
+import com.htmake.htbot.discord.util.ObjectMapperUtil;
+import com.htmake.htbot.global.cache.CacheFactory;
 import com.htmake.htbot.global.unirest.HttpClient;
 import com.htmake.htbot.global.unirest.impl.HttpClientImpl;
 import kotlin.Pair;
 import net.dv8tion.jda.api.events.interaction.component.ButtonInteractionEvent;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class MonsterKillAction {
 
     private final HttpClient httpClient;
     private final BattleUtil battleUtil;
-    private final BattleResultAction battleResultAction;
+    private final ObjectMapperUtil objectMapperUtil;
+
+    private final FieldBattleResultAction fieldBattleResultAction;
+    private final BossBattleResultAction bossBattleResultAction;
+
+    private final DungeonTypeCache dungeonTypeCache;
 
     public MonsterKillAction() {
         this.httpClient = new HttpClientImpl();
         this.battleUtil = new BattleUtil();
-        this.battleResultAction = new BattleResultAction();
+        this.objectMapperUtil = new ObjectMapperUtil();
+
+        this.fieldBattleResultAction = new FieldBattleResultAction();
+        this.bossBattleResultAction = new BossBattleResultAction();
+
+        this.dungeonTypeCache = CacheFactory.dungeonTypeCache;
     }
 
     public void execute(ButtonInteractionEvent event, PlayerStatus playerStatus, MonsterStatus monsterStatus) {
-        killMonster(event, playerStatus, monsterStatus);
-        countMonster(event, monsterStatus);
-    }
-
-    private void killMonster(ButtonInteractionEvent event, PlayerStatus playerStatus, MonsterStatus monsterStatus) {
         String playerId = event.getUser().getId();
 
         String message = monsterStatus.getName() + "을/를 처치했다!";
         battleUtil.updateSituation(playerId, message);
         battleUtil.editEmbed(event, playerStatus, monsterStatus, "progress");
 
-        battleResultAction.execute(event, monsterStatus.getId());
+        DungeonType dungeonType = dungeonTypeCache.get(playerId);
+        switch (dungeonType) {
+            case FIELD -> fieldBattleResultAction.execute(event, monsterStatus.getId());
+            case BOSS -> bossBattleResultAction.execute(event, monsterStatus.getId());
+        }
 
         battleUtil.removeCurrentBattleCache(playerId);
+
+        questMonsterCount(playerId, monsterStatus.getName());
     }
 
-    private void countMonster(ButtonInteractionEvent event, MonsterStatus monsterStatus) {
-        String playerId = event.getUser().getId();
+    private void questMonsterCount(String playerId, String monsterName) {
+        Map<String, Object> requestData = new HashMap<>();
+        requestData.put("name", monsterName);
 
         String endPoint = "/quest/monster/{player_id}";
         Pair<String, String> routeParam = new Pair<>("player_id", playerId);
+        String jsonBody = objectMapperUtil.mapper(requestData);
 
-        String requestBody = "{\"name\": \"" + monsterStatus.getName() + "\"}";
-
-        httpClient.sendPatchRequest(endPoint, routeParam, requestBody);
+        httpClient.sendPatchRequest(endPoint, routeParam, jsonBody);
     }
 }
