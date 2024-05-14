8022740d497a9d28583742389171f870c1d35856
hothuuk <kijkj123@gmail.com>
2024-05-14 14:47:10 +0900

    update :: 수정된 response 값에 맞게 수정

--- src/main/java/com/htmake/htbot/domain/dungeon/service/impl/BossDungeonInfoServiceImpl.java
+++ src/main/java/com/htmake/htbot/domain/dungeon/service/impl/BossDungeonInfoServiceImpl.java
@@ -3,16 +3,18 @@ package com.htmake.htbot.domain.dungeon.service.impl;
 import com.htmake.htbot.domain.dungeon.entity.Dungeon;
 import com.htmake.htbot.domain.dungeon.exception.DungeonNotFoundException;
 import com.htmake.htbot.domain.dungeon.presentation.data.response.BossDungeonInfoResponse;
+import com.htmake.htbot.domain.dungeon.presentation.data.response.DungeonKeyResponse;
 import com.htmake.htbot.domain.dungeon.repository.DungeonRepository;
 import com.htmake.htbot.domain.dungeon.service.BossDungeonInfoService;
 import com.htmake.htbot.domain.inventory.entity.Inventory;
 import com.htmake.htbot.domain.inventory.repository.InventoryRepository;
-import com.htmake.htbot.domain.misc.entity.Misc;
-import com.htmake.htbot.domain.misc.exception.MiscNotFoundException;
-import com.htmake.htbot.domain.misc.repository.MiscRepository;
+import com.htmake.htbot.domain.monster.entity.Monster;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @Transactional(readOnly = true)
@@ -21,25 +23,35 @@ public class BossDungeonInfoServiceImpl implements BossDungeonInfoService {
 
     private final DungeonRepository dungeonRepository;
     private final InventoryRepository inventoryRepository;
-    private final MiscRepository miscRepository;
 
     @Override
     public BossDungeonInfoResponse execute(String dungeonId, String playerId) {
         Dungeon dungeon = dungeonRepository.findById(dungeonId)
                 .orElseThrow(DungeonNotFoundException::new);
 
-        Misc misc = miscRepository.findById(dungeon.getKey())
-                .orElseThrow(MiscNotFoundException::new);
+        List<DungeonKeyResponse> dungeonKeyResponseList = dungeon.getDungeonKeyList().stream()
+                .map(dungeonKey -> {
+                    Inventory item = inventoryRepository.findByPlayerIdAndItemId(playerId, dungeonKey.getItemId())
+                            .orElse(null);
 
-        Inventory item = inventoryRepository.findByPlayerIdAndItemId(playerId, dungeon.getKey())
-                .orElse(null);
+                    int playerQuantity = item != null ? item.getQuantity() : 0;
 
-        int quantity = item != null ? item.getQuantity() : 0;
+                    return DungeonKeyResponse.builder()
+                            .name(dungeonKey.getName())
+                            .requireQuantity(dungeonKey.getQuantity())
+                            .playerQuantity(playerQuantity)
+                            .build();
+                })
+                .collect(Collectors.toList());
+
+        List<String> monsterNameList = dungeon.getMonsterList().stream()
+                .map(Monster::getName)
+                .collect(Collectors.toList());
 
         return BossDungeonInfoResponse.builder()
                 .name(dungeon.getName())
-                .dungeonKey(misc.getName())
-                .playerKeyQuantity(quantity)
+                .dungeonKeyList(dungeonKeyResponseList)
+                .monsterNameList(monsterNameList)
                 .build();
     }
 }
