d35251fcce81caaabef8e35c4b31b98df410cf2d
오진서 <s22030@gsm.hs.kr>
2024-05-28 16:55:33 +0900

    create :: Profession Controller

---
+++ src/main/java/com/htmake/htbot/domain/profession/presentation/ProfessionController.java
@@ -0,0 +1,40 @@
+package com.htmake.htbot.domain.profession.presentation;
+
+import com.htmake.htbot.domain.profession.presentation.data.JobPromotionInfoListResponse;
+import com.htmake.htbot.domain.profession.presentation.data.JobPromotionInfoResponse;
+import com.htmake.htbot.domain.profession.service.JobPromotionInfoService;
+import com.htmake.htbot.domain.profession.service.JobPromotionCompleteService;
+import com.htmake.htbot.domain.profession.service.PlayerJobInfoService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/profession")
+public class ProfessionController {
+
+    private final JobPromotionInfoService jobPromotionInfoService;
+    private final JobPromotionCompleteService jobPromotionCompleteService;
+    private final PlayerJobInfoService playerJobInfoService;
+
+    @GetMapping("/promotion/{player_id}/{job_name}")
+    public ResponseEntity<JobPromotionInfoResponse> quest(@PathVariable("player_id") String playerId, @PathVariable("job_name") String jobName) {
+        JobPromotionInfoResponse response = jobPromotionInfoService.execute(playerId, jobName);
+        return new ResponseEntity<>(response, HttpStatus.OK);
+    }
+
+    @GetMapping("/job/{player_id}")
+    public ResponseEntity<JobPromotionInfoListResponse> job(@PathVariable("player_id") String playerId) {
+        JobPromotionInfoListResponse response = playerJobInfoService.execute(playerId);
+        return new ResponseEntity<>(response, HttpStatus.OK);
+    }
+
+    @PatchMapping("/promotion/{player_id}/{job_name}")
+    public ResponseEntity<Void> questProgress(@PathVariable("player_id") String playerId,
+                                              @PathVariable("job_name") String jobName) {
+        jobPromotionCompleteService.execute(playerId, jobName);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+}
