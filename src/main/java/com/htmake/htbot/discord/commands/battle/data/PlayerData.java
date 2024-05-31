54500af03a29045bd3c064a88c1acdffd74b9a02
hothuuk <kijkj123@gmail.com>
2024-05-31 15:38:53 +0900

    create :: PlayerData

---
+++ src/main/java/com/htmake/htbot/discord/commands/battle/data/PlayerData.java
@@ -0,0 +1,19 @@
+package com.htmake.htbot.discord.commands.battle.data;
+
+import com.htmake.htbot.discord.commands.battle.data.status.extend.PlayerOriginalStatus;
+import com.htmake.htbot.discord.commands.battle.data.status.extend.PlayerStatus;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class PlayerData {
+
+    PlayerStatus playerStatus;
+
+    PlayerOriginalStatus playerOriginalStatus;
+}
