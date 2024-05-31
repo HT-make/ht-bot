dca361c740769a30a1a89317e656c0f48e86cb20
hothuuk <kijkj123@gmail.com>
2024-05-31 15:04:59 +0900

    update :: 매게변수와 반환 값 변경

--- src/main/java/com/htmake/htbot/discord/skillAction/skills/archer/ArrowRain.java
+++ src/main/java/com/htmake/htbot/discord/skillAction/skills/archer/ArrowRain.java
@@ -1,8 +1,11 @@
 package com.htmake.htbot.discord.skillAction.skills.archer;
 
-import com.htmake.htbot.discord.commands.battle.data.MonsterStatus;
-import com.htmake.htbot.discord.commands.battle.data.PlayerStatus;
+import com.htmake.htbot.discord.commands.battle.data.MonsterData;
+import com.htmake.htbot.discord.commands.battle.data.PlayerData;
+import com.htmake.htbot.discord.commands.battle.data.status.extend.MonsterStatus;
+import com.htmake.htbot.discord.commands.battle.data.status.extend.PlayerStatus;
 import com.htmake.htbot.discord.skillAction.skills.SkillStrategy;
+import com.htmake.htbot.discord.skillAction.type.SkillType;
 import kotlin.Pair;
 
 import java.util.ArrayList;
@@ -11,14 +14,17 @@ import java.util.List;
 public class ArrowRain implements SkillStrategy {
 
     @Override
-    public List<Pair<String, String>> execute(PlayerStatus player, MonsterStatus monster) {
-        List<Pair<String, String>> resultList = new ArrayList<>();
+    public List<Pair<String, SkillType>> execute(PlayerData playerData, MonsterData monsterData) {
+        List<Pair<String, SkillType>> resultList = new ArrayList<>();
 
-        int damage = critical((int) (player.getDamage() * 2.2), player.getCriticalDamage(), player.getCriticalChance() - 10);
-        int damageReceived = Math.max(0, damage - monster.getDefence());
-        monster.setHealth(Math.max(0, monster.getHealth() - damageReceived));
+        PlayerStatus playerStatus = playerData.getPlayerStatus();
+        MonsterStatus monsterStatus = monsterData.getMonsterStatus();
 
-        resultList.add(new Pair<>(String.valueOf(damageReceived), "ATTACK"));
+        int damage = critical((int) (playerStatus.getDamage() * 2.2), playerStatus.getCriticalDamage(), playerStatus.getCriticalChance() - 10);
+        int damageReceived = Math.max(0, damage - monsterStatus.getDefence());
+        monsterStatus.setHealth(Math.max(0, monsterStatus.getHealth() - damageReceived));
+
+        resultList.add(new Pair<>(String.valueOf(damageReceived), SkillType.ATTACK));
 
         return resultList;
     }
--- src/main/java/com/htmake/htbot/discord/skillAction/skills/archer/Breathing.java
+++ src/main/java/com/htmake/htbot/discord/skillAction/skills/archer/Breathing.java
@@ -1,8 +1,11 @@
 package com.htmake.htbot.discord.skillAction.skills.archer;
 
-import com.htmake.htbot.discord.commands.battle.data.MonsterStatus;
-import com.htmake.htbot.discord.commands.battle.data.PlayerStatus;
+import com.htmake.htbot.discord.commands.battle.data.MonsterData;
+import com.htmake.htbot.discord.commands.battle.data.PlayerData;
+import com.htmake.htbot.discord.commands.battle.data.status.extend.PlayerOriginalStatus;
+import com.htmake.htbot.discord.commands.battle.data.status.extend.PlayerStatus;
 import com.htmake.htbot.discord.skillAction.skills.SkillStrategy;
+import com.htmake.htbot.discord.skillAction.type.SkillType;
 import kotlin.Pair;
 
 import java.util.ArrayList;
@@ -11,13 +14,16 @@ import java.util.List;
 public class Breathing implements SkillStrategy {
 
     @Override
-    public List<Pair<String, String>> execute(PlayerStatus player, MonsterStatus monster) {
-        List<Pair<String, String>> resultList = new ArrayList<>();
+    public List<Pair<String, SkillType>> execute(PlayerData playerData, MonsterData monsterData) {
+        List<Pair<String, SkillType>> resultList = new ArrayList<>();
 
-        int healing = (int) (player.getDamage() * 1.5);
-        player.setHealth(player.getHealth() + healing);
+        PlayerStatus playerStatus = playerData.getPlayerStatus();
+        PlayerOriginalStatus playerOriginalStatus = playerData.getPlayerOriginalStatus();
 
-        resultList.add(new Pair<>(String.valueOf(healing), "HEAL"));
+        int healing = (int) (playerStatus.getDamage() * 1.5);
+        playerStatus.setHealth(Math.min(playerOriginalStatus.getHealth(), playerStatus.getHealth() + healing));
+
+        resultList.add(new Pair<>(String.valueOf(healing), SkillType.HEAL));
 
         return resultList;
     }
