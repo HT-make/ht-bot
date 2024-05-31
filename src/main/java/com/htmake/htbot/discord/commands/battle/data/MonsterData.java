2e7c544bd632c134f9b8493ece37defd5bb97fc4
hothuuk <kijkj123@gmail.com>
2024-05-31 15:38:42 +0900

    create :: MonsterData

---
+++ src/main/java/com/htmake/htbot/discord/commands/battle/data/MonsterData.java
@@ -0,0 +1,19 @@
+package com.htmake.htbot.discord.commands.battle.data;
+
+import com.htmake.htbot.discord.commands.battle.data.status.extend.MonsterOriginalStatus;
+import com.htmake.htbot.discord.commands.battle.data.status.extend.MonsterStatus;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class MonsterData {
+
+    MonsterStatus monsterStatus;
+
+    MonsterOriginalStatus monsterOriginalStatus;
+}
