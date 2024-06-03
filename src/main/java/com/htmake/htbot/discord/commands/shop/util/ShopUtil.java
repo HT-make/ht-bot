0c30dc32e98b583a48a3e15b7bc66d9892a3b1a7
오진서 <s22030@gsm.hs.kr>
2024-06-03 17:56:45 +0900

    update :: deleted errorMessage and BossShop event format

--- src/main/java/com/htmake/htbot/discord/commands/shop/util/ShopUtil.java
+++ src/main/java/com/htmake/htbot/discord/commands/shop/util/ShopUtil.java
@@ -1,30 +1,38 @@
 package com.htmake.htbot.discord.commands.shop.util;
 
+import com.htmake.htbot.discord.commands.shop.data.BossShopItem;
 import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.entities.MessageEmbed;
-import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
+import net.dv8tion.jda.api.events.interaction.component.StringSelectInteractionEvent;
 
 import java.awt.*;
+import java.util.Collections;
+import java.util.List;
 
 public class ShopUtil {
 
-    public void successPurchase(SlashCommandInteractionEvent event, int gold) {
+    public void successPurchase(StringSelectInteractionEvent event, int gold) {
         MessageEmbed embed = new EmbedBuilder()
                 .setColor(Color.GREEN)
                 .setTitle("아이템 구매에 성공했습니다!")
                 .setDescription("보유 잔액: " + gold)
                 .build();
 
-        event.replyEmbeds(embed).queue();
+        event.getHook().editOriginalComponents(Collections.emptyList()).queue();
+        event.getHook().editOriginalEmbeds(embed).queue();
     }
 
-    public void errorMessage(SlashCommandInteractionEvent event, String title, String message) {
-        MessageEmbed embed = new EmbedBuilder()
-                .setColor(Color.ORANGE)
-                .setTitle(":warning: " + title)
-                .setDescription(message)
-                .build();
+    public String format(List<BossShopItem> itemList) {
+        StringBuilder sb = new StringBuilder();
 
-        event.replyEmbeds(embed).queue();
+        for (BossShopItem item : itemList) {
+            sb.append("- ")
+                    .append(item.getName())
+                    .append(" - ")
+                    .append(item.getCoin())
+                    .append("코인\n");
+        }
+
+        return sb.toString();
     }
 }
