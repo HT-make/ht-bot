8fa8c2bb69b966f304dcc30cd99805a33a0c2315
오진서 <s22030@gsm.hs.kr>
2024-06-03 17:51:43 +0900

    create :: BossShopItemPurchaseService

---
+++ src/main/java/com/htmake/htbot/domain/shop/service/impl/BossShopItemPurchaseServiceImpl.java
@@ -0,0 +1,64 @@
+package com.htmake.htbot.domain.shop.service.impl;
+
+import com.htmake.htbot.domain.inventory.entity.Inventory;
+import com.htmake.htbot.domain.inventory.repository.InventoryRepository;
+import com.htmake.htbot.domain.player.entity.Player;
+import com.htmake.htbot.domain.player.exception.NotFoundPlayerException;
+import com.htmake.htbot.domain.player.repository.PlayerRepository;
+import com.htmake.htbot.domain.shop.entity.BossShop;
+import com.htmake.htbot.domain.shop.exception.NotEnoughBossCoinException;
+import com.htmake.htbot.domain.shop.exception.NotFoundItemException;
+import com.htmake.htbot.domain.shop.repository.BossShopRepository;
+import com.htmake.htbot.domain.shop.service.BossShopItemPurchaseService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class BossShopItemPurchaseServiceImpl implements BossShopItemPurchaseService {
+
+    private final BossShopRepository bossShopRepository;
+    private final InventoryRepository inventoryRepository;
+    private final PlayerRepository playerRepository;
+
+    @Override
+    public void execute(String playerId, String itemId) {
+
+        BossShop item = bossShopRepository.findById(itemId)
+                .orElseThrow(NotFoundItemException::new);
+
+        Player player = playerRepository.findById(playerId)
+                .orElseThrow(NotFoundPlayerException::new);
+
+        Inventory bossCoin = inventoryRepository.findByPlayerIdAndName(player.getId(), "보스 코인")
+                .orElseThrow(NotFoundItemException::new);
+
+        if (bossCoin.getQuantity() < item.getCoin()) {
+            throw new NotEnoughBossCoinException();
+        }
+
+        bossCoin.setQuantity(bossCoin.getQuantity() - item.getCoin());
+        if (bossCoin.getQuantity() == 0) {
+            inventoryRepository.delete(bossCoin);
+        }
+
+        Inventory existingItem = inventoryRepository.findByPlayerIdAndItemId(player.getId(), item.getId())
+                .orElse(null);
+
+        if (existingItem != null) {
+            existingItem.setQuantity(existingItem.getQuantity() + 1);
+            inventoryRepository.save(existingItem);
+        } else {
+            Inventory inventory = Inventory.builder()
+                    .itemId(item.getId())
+                    .player(player)
+                    .name(item.getName())
+                    .quantity(1)
+                    .build();
+
+            inventoryRepository.save(inventory);
+        }
+    }
+}
