49ff4a41adc0d7ea0526bb47ff24a2723c39caba
hothuuk <kijkj123@gmail.com>
2024-06-05 16:02:07 +0900

    create :: Invincible

---
+++ src/main/java/com/htmake/htbot/discord/skillAction/condition/extend/Invincible.java
@@ -0,0 +1,10 @@
+package com.htmake.htbot.discord.skillAction.condition.extend;
+
+import com.htmake.htbot.discord.skillAction.condition.Condition;
+
+public class Invincible extends Condition {
+
+    public Invincible(String id, String name, String emoji, int turn) {
+        super(id, name, emoji, turn);
+    }
+}
