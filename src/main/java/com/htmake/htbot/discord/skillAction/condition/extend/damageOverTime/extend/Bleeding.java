907b2394090c518ebbda1af5ed76eef2c52aeb92
hothuuk <kijkj123@gmail.com>
2024-06-11 17:43:46 +0900

    create :: Bleeding

---
+++ src/main/java/com/htmake/htbot/discord/skillAction/condition/extend/damageOverTime/extend/Bleeding.java
@@ -0,0 +1,26 @@
+package com.htmake.htbot.discord.skillAction.condition.extend.damageOverTime.extend;
+
+import com.htmake.htbot.discord.skillAction.condition.extend.damageOverTime.DamageOverTime;
+
+public class Bleeding extends DamageOverTime {
+
+    public Bleeding(int version) {
+        super("bleeding", "출혈" + getRomanNumeral(version), ":drop_of_blood:", 5, getValue(version));
+    }
+
+    private static String getRomanNumeral(int version) {
+        return switch (version) {
+            case 1 -> "I";
+            case 2 -> "II";
+            default -> throw new IllegalArgumentException("Invalid version: " + version);
+        };
+    }
+
+    private static double getValue(int version) {
+        return switch (version) {
+            case 1 -> 0.35;
+            case 2 -> 0.7;
+            default -> throw new IllegalArgumentException("Invalid version: " + version);
+        };
+    }
+}
