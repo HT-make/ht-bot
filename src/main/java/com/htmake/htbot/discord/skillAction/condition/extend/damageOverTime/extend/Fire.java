2c87d13d01a9004e33054f7b5cdf9e7b6642d964
hothuuk <kijkj123@gmail.com>
2024-06-11 17:43:59 +0900

    create :: Fire

---
+++ src/main/java/com/htmake/htbot/discord/skillAction/condition/extend/damageOverTime/extend/Fire.java
@@ -0,0 +1,28 @@
+package com.htmake.htbot.discord.skillAction.condition.extend.damageOverTime.extend;
+
+import com.htmake.htbot.discord.skillAction.condition.extend.damageOverTime.DamageOverTime;
+
+public class Fire extends DamageOverTime {
+
+    public Fire(int version) {
+        super("fire", "화상" + getRomanNumeral(version), ":fire:", 3, getValue(version));
+    }
+
+    private static String getRomanNumeral(int version) {
+        return switch (version) {
+            case 1 -> "I";
+            case 2 -> "II";
+            case 3 -> "III";
+            default -> throw new IllegalArgumentException("Invalid version: " + version);
+        };
+    }
+
+    private static double getValue(int version) {
+        return switch (version) {
+            case 1 -> 0.5;
+            case 2 -> 0.9;
+            case 3 -> 1.5;
+            default -> throw new IllegalArgumentException("Invalid version: " + version);
+        };
+    }
+}
