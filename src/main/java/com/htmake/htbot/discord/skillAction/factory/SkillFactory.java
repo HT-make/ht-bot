2d0dbc0b604c1071f33a472c178a36d4e670caf4
hothuuk <kijkj123@gmail.com>
2024-06-27 12:07:29 +0900

    add :: new skills

--- src/main/java/com/htmake/htbot/discord/skillAction/factory/SkillFactory.java
+++ src/main/java/com/htmake/htbot/discord/skillAction/factory/SkillFactory.java
@@ -1,14 +1,18 @@
 package com.htmake.htbot.discord.skillAction.factory;
 
 import com.htmake.htbot.discord.skillAction.BasicSkill;
-import com.htmake.htbot.discord.skillAction.skills.archer.class1.*;
-import com.htmake.htbot.discord.skillAction.skills.archer.class2.*;
-import com.htmake.htbot.discord.skillAction.skills.archer.class3.*;
-import com.htmake.htbot.discord.skillAction.skills.archer.class4.*;
 import com.htmake.htbot.discord.skillAction.skills.warrior.class1.*;
 import com.htmake.htbot.discord.skillAction.skills.warrior.class2.*;
 import com.htmake.htbot.discord.skillAction.skills.warrior.class3.*;
 import com.htmake.htbot.discord.skillAction.skills.warrior.class4.*;
+import com.htmake.htbot.discord.skillAction.skills.archer.class1.*;
+import com.htmake.htbot.discord.skillAction.skills.archer.class2.*;
+import com.htmake.htbot.discord.skillAction.skills.archer.class3.*;
+import com.htmake.htbot.discord.skillAction.skills.archer.class4.*;
+import com.htmake.htbot.discord.skillAction.skills.wizard.class1.*;
+import com.htmake.htbot.discord.skillAction.skills.wizard.class2.*;
+import com.htmake.htbot.discord.skillAction.skills.wizard.class3.*;
+import com.htmake.htbot.discord.skillAction.skills.wizard.class4.*;
 import org.springframework.stereotype.Component;
 
 import java.util.HashMap;
@@ -71,5 +75,29 @@ public class SkillFactory {
         skillMap.put("S2403", new BasicSkill(new S2403()));
         skillMap.put("S2404", new BasicSkill(new S2404()));
         skillMap.put("S2405", new BasicSkill(new S2405()));
+
+        //WIZARD-Class1
+        skillMap.put("S3101", new BasicSkill(new S3101()));
+        skillMap.put("S3102", new BasicSkill(new S3102()));
+        skillMap.put("S3103", new BasicSkill(new S3103()));
+        skillMap.put("S3104", new BasicSkill(new S3104()));
+        //WIZARD-Class2
+        skillMap.put("S3201", new BasicSkill(new S3201()));
+        skillMap.put("S3202", new BasicSkill(new S3202()));
+        skillMap.put("S3203", new BasicSkill(new S3203()));
+        skillMap.put("S3204", new BasicSkill(new S3204()));
+        skillMap.put("S3205", new BasicSkill(new S3205()));
+        //WIZARD-Class3
+        skillMap.put("S3301", new BasicSkill(new S3301()));
+        skillMap.put("S3302", new BasicSkill(new S3302()));
+        skillMap.put("S3303", new BasicSkill(new S3303()));
+        skillMap.put("S3304", new BasicSkill(new S3304()));
+        skillMap.put("S3305", new BasicSkill(new S3305()));
+        skillMap.put("S3306", new BasicSkill(new S3306()));
+        //WIZARD-Class4
+        skillMap.put("S3401", new BasicSkill(new S3401()));
+        skillMap.put("S3402", new BasicSkill(new S3402()));
+        skillMap.put("S3403", new BasicSkill(new S3403()));
+        skillMap.put("S3404", new BasicSkill(new S3404()));
     }
 }
