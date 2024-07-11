991d395cd83ecd9370bf0af1b6a571fac960776f
hothuuk <kijkj123@gmail.com>
2024-07-11 12:05:02 +0900

    add :: conditions

--- src/main/java/com/htmake/htbot/discord/skillAction/factory/ConditionFactory.java
+++ src/main/java/com/htmake/htbot/discord/skillAction/factory/ConditionFactory.java
@@ -1,9 +1,8 @@
 package com.htmake.htbot.discord.skillAction.factory;
 
 import com.htmake.htbot.discord.skillAction.condition.Condition;
-import com.htmake.htbot.discord.skillAction.condition.extend.etc.Faint;
-import com.htmake.htbot.discord.skillAction.condition.extend.etc.Invincible;
-import com.htmake.htbot.discord.skillAction.condition.extend.etc.Thorn;
+import com.htmake.htbot.discord.skillAction.condition.extend.damageOverTime.extend.Toxic;
+import com.htmake.htbot.discord.skillAction.condition.extend.etc.*;
 import com.htmake.htbot.discord.skillAction.condition.extend.buff.extend.*;
 import com.htmake.htbot.discord.skillAction.condition.extend.damageOverTime.extend.Bleeding;
 import com.htmake.htbot.discord.skillAction.condition.extend.damageOverTime.extend.Fire;
@@ -38,12 +37,16 @@ public class ConditionFactory {
         conditionMap.put("hit_2", new Hit(2));
         conditionMap.put("hit_3", new Hit(3));
 
+        conditionMap.put("perfect_accuracy_1", new PerfectAccuracy());
+
         conditionMap.put("power_1", new Power(1));
         conditionMap.put("power_2", new Power(2));
         conditionMap.put("power_3", new Power(3));
+        conditionMap.put("power_4", new Power(4));
 
         conditionMap.put("protect_1", new Protect(1));
         conditionMap.put("protect_2", new Protect(2));
+        conditionMap.put("protect_3", new Protect(3));
 
         // DOT
         conditionMap.put("bleeding_1", new Bleeding(1));
@@ -52,15 +55,25 @@ public class ConditionFactory {
         conditionMap.put("fire_1", new Fire(1));
         conditionMap.put("fire_2", new Fire(2));
         conditionMap.put("fire_3", new Fire(3));
+        conditionMap.put("fire_4", new Fire(4));
 
         conditionMap.put("poison_1", new Poison(1));
         conditionMap.put("poison_2", new Poison(2));
 
+        conditionMap.put("toxic_1", new Toxic(1));
+
         // OTHERS
         conditionMap.put("faint_1", new Faint(1));
         conditionMap.put("faint_2", new Faint(2));
+        conditionMap.put("faint_3", new Faint(3));
 
-        conditionMap.put("invincible", new Invincible());
+        conditionMap.put("invincible_1", new Invincible());
+
+        conditionMap.put("life_steal_1", new LifeSteal(1));
+        conditionMap.put("life_steal_2", new LifeSteal(2));
+
+        conditionMap.put("regeneration_1", new Regeneration(1));
+        conditionMap.put("regeneration_2", new Regeneration(2));
 
         conditionMap.put("thorn_1", new Thorn(1));
         conditionMap.put("thorn_2", new Thorn(2));
