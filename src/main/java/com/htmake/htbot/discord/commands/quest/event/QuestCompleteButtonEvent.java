5a50b37adcebad9db72cc03fcb816086bf117b97
hothuuk <kijkj123@gmail.com>
2024-07-14 15:44:35 +0900

    update :: quest events

--- src/main/java/com/htmake/htbot/discord/commands/quest/event/QuestCompleteButtonEvent.java
+++ src/main/java/com/htmake/htbot/discord/commands/quest/event/QuestCompleteButtonEvent.java
@@ -1,52 +1,17 @@
 package com.htmake.htbot.discord.commands.quest.event;
 
-import com.htmake.htbot.discord.util.ErrorUtil;
-import com.htmake.htbot.global.unirest.HttpClient;
-import com.htmake.htbot.global.unirest.impl.HttpClientImpl;
-import com.mashape.unirest.http.HttpResponse;
-import com.mashape.unirest.http.JsonNode;
-import kotlin.Pair;
-import net.dv8tion.jda.api.EmbedBuilder;
-import net.dv8tion.jda.api.entities.MessageEmbed;
+import com.htmake.htbot.discord.commands.quest.action.QuestDialogueAction;
 import net.dv8tion.jda.api.events.interaction.component.ButtonInteractionEvent;
-
-import java.awt.*;
-import java.util.Collections;
 
 public class QuestCompleteButtonEvent {
 
-    private final HttpClient httpClient;
-    private final ErrorUtil errorUtil;
+    private final QuestDialogueAction questDialogueAction;
 
     public QuestCompleteButtonEvent() {
-        this.httpClient = new HttpClientImpl();
-        this.errorUtil = new ErrorUtil();
+        this.questDialogueAction = new QuestDialogueAction();
     }
 
     public void execute(ButtonInteractionEvent event) {
-        HttpResponse<JsonNode> response = request(event.getUser().getId());
-
-        if (response.getStatus() == 200) {
-            requestSuccess(event);
-        } else {
-            errorUtil.sendError(event.getHook(), "퀘스트를 완료하지 못했습니다.", "목표를 달성하고 다시 시도해 주세요.");
-        }
-    }
-
-    private HttpResponse<JsonNode> request(String playerId) {
-        String endPoint = "/quest/progress/{player_id}";
-        Pair<String, String> routeParam = new Pair<>("player_id", playerId);
-        return httpClient.sendPostRequest(endPoint, routeParam);
-    }
-
-    public void requestSuccess(ButtonInteractionEvent event) {
-        MessageEmbed embed = new EmbedBuilder()
-                .setColor(Color.GREEN)
-                .setTitle(":tada: 퀘스트를 완료했습니다.")
-                .setDescription("다음 퀘스트를 확인해 주세요.")
-                .build();
-
-        event.getHook().editOriginalComponents(Collections.emptyList()).queue();
-        event.getHook().editOriginalEmbeds(embed).queue();
+        questDialogueAction.execute(event);
     }
 }
--- src/main/java/com/htmake/htbot/discord/commands/quest/event/QuestSlashEvent.java
+++ src/main/java/com/htmake/htbot/discord/commands/quest/event/QuestSlashEvent.java
@@ -1,124 +1,57 @@
 package com.htmake.htbot.discord.commands.quest.event;
 
+import com.htmake.htbot.discord.commands.quest.action.QuestDetailAction;
+import com.htmake.htbot.discord.commands.quest.action.QuestDialogueAction;
 import com.htmake.htbot.discord.util.ErrorUtil;
-import com.htmake.htbot.discord.util.FormatUtil;
 import com.htmake.htbot.global.unirest.HttpClient;
 import com.htmake.htbot.global.unirest.impl.HttpClientImpl;
 import com.mashape.unirest.http.HttpResponse;
 import com.mashape.unirest.http.JsonNode;
 import kotlin.Pair;
-import net.dv8tion.jda.api.EmbedBuilder;
-import net.dv8tion.jda.api.entities.MessageEmbed;
-import net.dv8tion.jda.api.entities.User;
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
-import net.dv8tion.jda.api.interactions.components.buttons.Button;
-import org.json.JSONArray;
 import org.json.JSONObject;
-
-import java.awt.*;
 
 public class QuestSlashEvent {
 
     private final HttpClient httpClient;
     private final ErrorUtil errorUtil;
 
+    private final QuestDetailAction questDetailAction;
+    private final QuestDialogueAction questDialogueAction;
+
     public QuestSlashEvent() {
         this.httpClient = new HttpClientImpl();
         this.errorUtil = new ErrorUtil();
+
+        this.questDetailAction = new QuestDetailAction();
+        this.questDialogueAction = new QuestDialogueAction();
     }
 
     public void execute(SlashCommandInteractionEvent event) {
         HttpResponse<JsonNode> response = request(event.getUser().getId());
-        
+
         if (response.getStatus() == 200) {
-            JSONObject questObject = response.getBody().getObject();
-            requestSuccess(event, questObject);
+            JSONObject checkObject = response.getBody().getObject();
+            requestSuccess(event, checkObject);
         } else {
-            errorUtil.sendError(event, "퀘스트를 불러올 수 없습니다.", "잠시 후 다시 이용해주세요.");
+            errorUtil.sendError(event, "퀘스트", "퀘스트를 불러올 수 없습니다.");
         }
     }
 
     private HttpResponse<JsonNode> request(String playerId) {
-        String endPoint = "/quest/{player_id}";
+        String endPoint = "/quest/dialogue/check/{player_id}";
         Pair<String, String> routeParam = new Pair<>("player_id", playerId);
         return httpClient.sendGetRequest(endPoint, routeParam);
     }
 
-    private void requestSuccess(SlashCommandInteractionEvent event, JSONObject questObject) {
-        MessageEmbed embed = buildEmbed(questObject, event.getUser());
+    private void requestSuccess(SlashCommandInteractionEvent event, JSONObject checkObject) {
+        boolean readDialogue = checkObject.getBoolean("readDialogue");
 
-        Button completeButton = Button.primary("quest-complete", "완료").asDisabled();
-
-        if (validTargetQuantity(questObject)) {
-            completeButton = Button.primary("quest-complete", "완료").asEnabled();
+        if (readDialogue) {
+            questDetailAction.execute(event);
+        } else {
+            questDialogueAction.execute(event);
         }
-
-        event.replyEmbeds(embed)
-                .addActionRow(
-                        completeButton,
-                        Button.danger("cancel", "닫기")
-                )
-                .queue();
-    }
-
-    private MessageEmbed buildEmbed(JSONObject questObject, User user) {
-        String profileUrl = user.getAvatarUrl() != null ? user.getAvatarUrl() : user.getDefaultAvatarUrl();
-
-        return new EmbedBuilder()
-                .setColor(Color.GREEN)
-                .setAuthor(user.getName(), null, profileUrl)
-                .setTitle(":scroll: 퀘스트")
-                .addField(questObject.getString("title"), questObject.getString("description"), false)
-                .addField(":dart: 목표", targetFormat(questObject), false)
-                .addField(":purse: 보상", rewardFormat(questObject), false)
-                .build();
-    }
-
-    private String targetFormat(JSONObject questObject) {
-        JSONArray targetArray = questObject.getJSONArray("targetList");
-        StringBuilder sb = new StringBuilder();
-
-        for (int i = 0; i < targetArray.length(); i++) {
-            JSONObject targetObject = targetArray.getJSONObject(i);
-            sb.append(targetObject.getString("name")).append(" (")
-                    .append(targetObject.getInt("currentQuantity")).append("/")
-                    .append(targetObject.getInt("requiredQuantity")).append(")\n");
-        }
-
-        return sb.toString();
-    }
-
-    private String rewardFormat(JSONObject questObject) {
-        JSONArray rewardArray = questObject.getJSONArray("rewardList");
-        StringBuilder sb = new StringBuilder();
-
-        sb.append(FormatUtil.decimalFormat(questObject.getInt("gold"))).append(" 골드\n")
-                .append(FormatUtil.decimalFormat(questObject.getInt("exp"))).append(" 경험치\n");
-
-        for (int i = 0; i < rewardArray.length(); i++) {
-            JSONObject rewardObject = rewardArray.getJSONObject(i);
-            sb.append(rewardObject.getString("name")).append(" ")
-                    .append(rewardObject.getInt("quantity")).append("개\n");
-        }
-
-        return sb.toString();
-    }
-
-    private boolean validTargetQuantity(JSONObject questObject) {
-        JSONArray targetArray = questObject.getJSONArray("targetList");
-
-        for (int i = 0; i < targetArray.length(); i++) {
-            JSONObject targetObject = targetArray.getJSONObject(i);
-
-            int requiredQuantity = targetObject.getInt("requiredQuantity");
-            int currentQuantity = targetObject.getInt("currentQuantity");
-
-            if (currentQuantity < requiredQuantity) {
-                return false;
-            }
-        }
-
-        return true;
     }
 }
 
